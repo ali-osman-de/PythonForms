6ffe255f74480a21944e46625c427841491b796f
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2022-09-03 21:59:50 +0300

    Create date.py

---
+++ date.py
@@ -0,0 +1,39 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QDate, QTime, QDateTime, Qt
+import sys
+from DateForm import Ui_MainWindow
+
+
+class myApp(QtWidgets.QMainWindow):
+    
+    def __init__(self):
+        super(myApp, self).__init__()
+    
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.btnCalculate.clicked.connect(self.calculate)
+        
+
+    def calculate(self):
+        start = self.ui.dateStart.date()
+        end = self.ui.dateEnd.date()
+        print(start, end)
+
+        print('Days in month: {0}'.format(start.daysInMonth()))
+        print('Days in Year: {0}'.format(start.daysInYear()))
+
+        print('total days: {0}'.format(start.daysTo(end)))
+
+        now = QDate.currentDate()
+
+        print('total days from now: {0}'.format(start.daysTo(now)))
+
+
+def app():
+    app = QtWidgets.QApplication(sys.argv)
+    win = myApp()
+    win.show()
+    sys.exit(app.exec_())
+
+app()
